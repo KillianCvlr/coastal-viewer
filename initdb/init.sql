0d6ef240c5989a2003b1994fe96ac38ffd0007e9
KillianCvlr <cavalier.killian@gmail.com>
2025-07-17 10:00:52 +0200

    changed database

--- initdb/init.sql
+++ initdb/init.sql
@@ -1,21 +1,24 @@
 CREATE EXTENSION IF NOT EXISTS postgis;
 
-CREATE TABLE IF NOT EXISTS photos (
-  id SERIAL PRIMARY KEY,
-  filename TEXT UNIQUE,
-  filepath TEXT,
-  datetime TIMESTAMP,
-  location GEOMETRY(Point, 4326),
-  description TEXT
-);
-
 CREATE TABLE  IF NOT EXISTS field_surveys (
   id SERIAL PRIMARY KEY,
   survey_name TEXT NOT NULL,
-  location GEOMETRY(Point, 4326),
+  location GEOGRAPHY(Point, 4326),
   datetime TIMESTAMP,
   comment TEXT,
   abovewater_folder TEXT,
   underwater_folder TEXT,
   linking_file TEXT
 );
+
+CREATE TABLE IF NOT EXISTS photos (
+  id SERIAL PRIMARY KEY,
+  filename TEXT,
+  filepath TEXT UNIQUE,
+  datetime TIMESTAMP,
+  location GEOGRAPHY(Point, 4326),
+  is_underwater BOOLEAN DEFAULT FALSE,
+  description TEXT,
+  survey_id INTEGER REFERENCES field_surveys(id) ON DELETE CASCADE
+);
+
